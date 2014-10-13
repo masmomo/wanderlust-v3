2f1525dd92017c0381ab6504e91b5b9ec36b5e5e
Dimas Nuh Putra <dimas@antikode.com>
2014-10-13 13:27:43 +0700

    Career

--- pages_/career/index.php
+++ pages_/career/index.php
@@ -26,6 +26,7 @@ function get_career(){
 /* --- CALL FUNCTION --- */
 $count_career = count_career();
 $data_career  = get_career();
+$modal_career = get_career();
 ?>
 
     <div class="container">
@@ -36,14 +37,14 @@ $data_career  = get_career();
 		  <?php 
 		  if($count_career['rows'] > 0){
 		     
-			 foreach($data_career as $data_career){
+			 foreach($data_career as $key=>$data_career){
           ?>
 
           <div class="col-xs-4">
-            <a data-toggle="modal" href="#myModal">
+            <a data-toggle="modal" href="#myModal-<?php echo $key;?>">
               <div class="career-box">
                 <h3 class="h5 m_b_15"><?php echo $data_career['career_name'];?></h3>
-                <p><?php echo $data_career['description'];?></p>
+                <p><?php echo substr($data_career['description'], 0, 150).'..';?></p>
               </div>
             </a>
           </div>
@@ -56,28 +57,29 @@ $data_career  = get_career();
         </div>
       </div><!--.content-->
     </div><!--.container.main-->
-
-    <div class="modal fade career-modal" id="myModal">
+    
+    <?php
+    foreach($modal_career as $key_modal=>$data_career){
+	?>
+    
+    <div class="modal fade career-modal" id="myModal-<?php echo $key_modal;?>">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-body">
-            <h4 class="m_b_20">Job Title</h4>
-            <p style="color: #999">In five years, Wanderlust has gone from an idea to a mature, growing organization. We’re looking for an exceptional person to join the senior team and oversee finance and business operations with us.
-              <br><br>
-              The VP of Operations and Finance will report directly to the CEO. They will oversee finances, human resources, office administration, and operational aspects of payments and international expansion. They will ensure that the organization runs smoothly and efficiently.
-              <br><br>
-              The operational duties are vital, and so is cultural fit. Kickstarter is a place dedicated to helping creative projects come to life. Want to work at a “hot start-up”? This is probably not the job for you. We're working hard to build something independent and longterm — not prepping for an IPO or sale. We are hungry, we are passionate, and our mission truly comes first.
-              <br><br>
-              Sound like an organization you’d like to be a part of? Reach out. We can’t wait to hear from you.</p>
-          </div>
+            <h4 class="m_b_20"><?php echo $data_career['career_name'];?></h4>
+            <p style="color: #999"><?php echo preg_replace("/\n/","\n<br>",$data_career['description']);?></div>
           <div class="p_20">
             <p><strong style="color: #000000">Interested?</strong>
               <br>
-              Send a little something about yourself to <a href="mailto:hello@mywanderlustories.com">hello@mywanderlustories.com</a> with links to your work. This position is full-time, on-site at our awesome office in Greenpoint, Brooklyn. Your compensation will include equity and health care.</p>
+              Send a little something about yourself to <a href="mailto:<?php echo $info['email'];?>"><?php echo $info['email'];?></a> with links to your work. This position is full-time, on-site at our awesome office in Greenpoint, Brooklyn. Your compensation will include equity and health care.</p>
           </div>
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+    
+    <?php
+	}
+	?>
 
 
 <?php
